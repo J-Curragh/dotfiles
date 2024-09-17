49de78b638027798c482e4937979fe6f556950ff
Jake Curragh <jake@jcli.codes>
2024-09-17 06:33:02 +0100

    include neovim

---
+++ .config/nvim/lua/lazy_setup.lua
@@ -0,0 +1,31 @@
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    version = "^4",
+    import = "astronvim.plugins",
+    opts = {
+      mapleader = ";",
+      maplocalleader = ";",
+      icons_enabled = true,
+      pin_plugins = true,
+      update_notifications = true,
+    },
+  },
+  { import = "community" },
+  { import = "plugins" },
+} --[[@as LazySpec]], {
+  install = { colorscheme = { "astrotheme", "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins, add more to your liking
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+} --[[@as LazyConfig]])
