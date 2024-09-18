493d6497d133fc9ad194c1795aa2e1829c349ad1
Jake Curragh <jake@jcli.codes>
2024-09-18 22:53:14 +0100

    solidify colours

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -16,6 +16,14 @@ return {
         },
       },
     },
+    config = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>c",
+        ":lua require('bufdelete').bufdelete(0, false)<CR>",
+        { noremap = true, silent = true, desc = "Delete buffer" }
+      )
+    end,
   },
 
   -- Disable plugins (e.g.)
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,44 @@
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "markdown",
-      "markdown_inline",
-      "bash",
-      "python",
-      "javascript",
-      "typescript",
-      "tsx",
-      "css",
-      "scss",
-      "html",
-      "json",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "python",
+        "javascript",
+        "typescript",
+        "tsx",
+        "css",
+        "scss",
+        "html",
+        "json",
+        "yaml",
+      },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      enable = true,
+      min_window_height = 25,
+      line_numbers = true,
+    },
+    config = function(_, opts)
+      local ok, context = pcall(require, "treesitter-context")
+      if not ok then return end
+
+      context.setup(opts)
+      vim.api.nvim_set_keymap(
+        "n",
+        "[c",
+        "<cmd>lua require('treesitter-context').go_to_context(vim.v.count1)<cr>",
+        { noremap = true, silent = true, desc = "Previous context" }
+      )
+    end,
   },
 }
